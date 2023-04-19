db4f78c2e975fc666681b7d00f3896e89be8fa5e
AlexisCubilla <alexiscubilla500@gmail.com>
2023-04-19 16:47:00 -0400

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ComputeEngine } from '@cortex-js/compute-engine'
+
+declare const MathfieldElement: any;
+
+// import { MathfieldElement } from 'mathlive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'mathlive';
+  ce2: any
+
+  mfe: any
+  constructor(
+
+  ) {
+    this.mfe = new MathfieldElement();
+  }
+
+  ngAfterViewInit(): void {
+    const mfe = new MathfieldElement()
+    const ce = new ComputeEngine({
+      latexDictionary: [
+        //@ts-ignore
+        ...ComputeEngine.getLatexDictionary(),
+        {
+          trigger: ['\\smallfrac'],
+          // trigger: ['#1/#2'],
+          //@ts-ignore
+          parse: (parser) => {
+            return [
+              'smallfrac',
+              parser.matchRequiredLatexArgument() ?? ['Error', "'missing'"],
+              parser.matchRequiredLatexArgument() ?? ['Error', "'missing'"],
+            ];
+          },
+        },
+      ],
+    });
+
+    let ent = '';
+    let ent2 = '';
+
+    let doc = document.getElementById(`global-expression-math-field`)
+
+    mfe.addEventListener('input', (e: any) => {
+
+      ent = e.target.getValue('latex-expanded')
+
+      ent2 = e.target.getValue('math-json')
+
+
+      console.log('latex: ', ent);
+
+      console.log('mathjson: ', ent2);
+
+    })
+
+    mfe.setOptions({
+      macros: {
+        ...this.mfe.getOptions('macros'),
+        'smallfrac': {
+          args: 2,
+          // def: '{}^{#1}\\!\\!/\\!{}_{#2}'
+          def: '#1/#2'
+        },
+      },
+      inlineShortcuts: {
+        ...this.mfe.getOptions('inlineShortcuts'),
+        // node: '{#1}_{\\left\\lbrack#2\\right\\rbrack}^{#3}({#4})'
+        // node:'\\mathrm{smallfrac}({#1},{#2})'
+        node: '\\smallfrac{#@}{#?}'
+      },
+      computeEngine: ce
+    })
+
+    doc?.appendChild(mfe)
+
+
+  }
 }
